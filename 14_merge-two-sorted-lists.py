d01498752534ca0972431b44df9c1f834d291d36
nureesong <nuri3136@naver.com>
2021-11-19 17:01:43 +0900

    modify the file

--- 14_merge-two-sorted-lists.py
+++ 14_merge-two-sorted-lists.py
@@ -9,7 +9,6 @@ q1, q2 = [1,2,4], [1,3,4]
 q3, q4 = [1,1,2,3], [2,4,6]
 q5, q6 = [], [1,1,3]
 
-
 # 1. while loop(My approach)
 def mergeTwoLists(l1, l2):
     if not(l1) and not(l2):
@@ -43,19 +42,26 @@ def mergeTwoLists(l1, l2):
     merged.append(max(i,j))
     return merged
 
+
 print('======= while loop ========')
 print(mergeTwoLists(q1,q2))
 print(mergeTwoLists(q3,q4))
 print(mergeTwoLists(q5,q6))
 
-
-# 2. Recursion
-
-
-
-
 '''
 list끼리 또는 deque끼리는 + 로 병합 가능
 list + deque는 TypeError 발생
-
 '''
+
+
+# 2. Recursion (Leetcode)
+# class Solution:
+#     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+#         if (not l1) or (l2 and l1.val > l2.val):
+#             l1, l2 = l2, l1
+#         if l1:
+#             print(f'before recursion: {l1.next}')
+#             print(f'l1.val: {l1.val}')
+#             l1.next = self.mergeTwoLists(l1.next, l2)
+#             print(f'after recursion: {l1.next}')
+#         return l1
